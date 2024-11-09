433b8bcc18b19523d352bd5a66534c73863a829c
naiemjoy1 <naiemunhasan@gmail.com>
2024-11-09 12:27:59 +0600

    array end

---
+++ arrayEnd.js
@@ -0,0 +1,43 @@
+// You are given two integers n and x. You have to construct an array of positive integers nums of size n where for every 0 <= i < n - 1, nums[i + 1] is greater than nums[i], and the result of the bitwise AND operation between all elements of nums is x.
+
+// Return the minimum possible value of nums[n - 1].
+
+// Example 1:
+
+// Input: n = 3, x = 4
+
+// Output: 6
+
+// Explanation:
+
+// nums can be [4,5,6] and its last element is 6.
+
+// Example 2:
+
+// Input: n = 2, x = 7
+
+// Output: 15
+
+// Explanation:
+
+// nums can be [7,15] and its last element is 15.
+
+// Constraints:
+
+// 1 <= n, x <= 108
+
+var minEnd = function (n, x) {
+  let nums = [x];
+
+  for (let i = 1; i < n; i++) {
+    let nextNum = nums[i - 1] + 1;
+    while ((nextNum & x) !== x) {
+      nextNum++;
+    }
+    nums.push(nextNum);
+  }
+  return nums[n - 1];
+};
+
+console.log(minEnd(3, 4));
+console.log(minEnd(2, 7));
