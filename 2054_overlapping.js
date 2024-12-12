0bf794f0b3c977a53970fe3affa34272cace093d
naiemjoy1 <naiemunhasan@gmail.com>
2024-12-12 11:02:21 +0600

    2558

--- 2054_overlapping.js
+++ 2054_overlapping.js
@@ -73,18 +73,18 @@ console.log(
     [4, 5, 2],
     [2, 4, 3],
   ])
-); // Output: 4
+);
 console.log(
   maxTwoEvents([
     [1, 3, 2],
     [4, 5, 2],
     [1, 5, 5],
   ])
-); // Output: 5
+);
 console.log(
   maxTwoEvents([
     [1, 5, 3],
     [1, 5, 1],
     [6, 6, 5],
   ])
-); // Output: 8
+);
